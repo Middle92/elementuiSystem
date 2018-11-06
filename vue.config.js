1e0589ce5fa1b1d78810508f32548681de48856c
holgor <840804613@qq.com>
2018-11-06 13:51:07 +0800

    更新配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const path = require('path')
+
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+function assetsPath(_path) {
+  const assetsSubDirectory = 'static'
+  return path.posix.join(assetsSubDirectory, _path)
+}
+
 module.exports = {
   /** 区分打包环境与开发环境
    * process.env.NODE_ENV==='production'  (打包环境)
@@ -29,11 +41,46 @@ module.exports = {
     //更改webpack的loader配置
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
+    svgRule.include.add(resolve('src/icons'))
     svgRule
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
       .options({
         symbolId: 'icon-[name]'
+      })
+
+    const imgRule = config.module.rule('img')
+    imgRule.uses.clear()
+    imgRule.exclude.add(resolve('src/icons'))
+    imgRule
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: 10000,
+        name: assetsPath('img/[name].[hash:7].[ext]')
+      })
+
+    const mediaRule = config.module.rule('media')
+    mediaRule.uses.clear()
+    mediaRule
+      .test(/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: 10000,
+        name: assetsPath('media/[name].[hash:7].[ext]')
+      })
+
+    const fontsRule = config.module.rule('fonts')
+    fontsRule.uses.clear()
+    fontsRule
+      .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: 10000,
+        name: assetsPath('fonts/[name].[hash:7].[ext]')
       })
   },
 }
